63da63338d45bfa719c7fc1f3af1a8ed09c5263e
Bethany Drake <bethanyd@thoughtworks.com>
2020-10-08 15:02:00 +1100

    when you hit endpoint, update the database and return updated vote counts

--- main.js
+++ main.js
@@ -11,29 +11,98 @@ var corsOptions = {
 
 }
 app.use(cors(corsOptions))
+AWS.config.update({region: "ap-southeast-2"})
 
-AWS.config.getCredentials(function(err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log("Access key:", AWS.config.credentials.accessKeyId);
-  }
-});
 app.use(bodyParser.json());
 app.options('*', cors(corsOptions))
 
-app.post('/vote', function (req, res) {
+var docClient = new AWS.DynamoDB.DocumentClient();
+function updateVoteCount(winner, loser, newCount) {
+  console.log("updateVoteCount" + JSON.stringify({winner, loser, newCount}));
+  var params = {
+      TableName :"drawfee-2",
+      Key:{
+          winner, loser
+      },
+      UpdateExpression: "set voteCount = voteCount + :val",
+      ExpressionAttributeValues:{
+        ":val":1
+    },
+      ReturnValues:"UPDATED_NEW"
+  };
+  docClient.update(params, function(err, data) {
+      if (err) {
+          console.log( "Unable to update item: " + "\n" + JSON.stringify(err, undefined, 2));
+      } else {
+        // console.log("update succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
+      }
+  });
+}
+
+function create(winner, loser) {
+  console.log("creating" + JSON.stringify({winner, loser}));
+  var params = {
+      TableName :"drawfee-2",
+      Item:{
+          winner, loser, voteCount: 0
+      },
+  };
+  docClient.put(params, function(err, data) {
+      if (err) {
+          console.log( "Unable to add item: " + "\n" + JSON.stringify(err, undefined, 2));
+      } else {
+        // console.log("PutItem succeeded: " + "\n" + JSON.stringify(data, undefined, 2));
+      }
+  });
+}
+
+async function getVotes(winner, loser) {
+  console.log("getVotes" + JSON.stringify({winner, loser}));
+  var table = "drawfee-2";
+
+  var params = {
+      TableName: table,
+      Key:{
+          winner, loser
+      }
+  };
+  let resolve;
+  const promise = new Promise((res) => {
+    resolve = res
+  })
+  docClient.get(params, function(err, data) {
+    if (err) {
+      err.statusCode
+      console.log( "Unable to get item: " + "\n" + JSON.stringify(err, undefined, 2));
+      create(winner, loser)
+    resolve(0)
+  } else {
+    if (data.Item && data.Item.voteCount !== undefined) {
+      resolve(data.Item.voteCount )
+
+    } else {
+      create(winner, loser)
+      resolve(0)
+    }
+  }
+
+  });
+  return promise;
+}
+
+app.post('/vote', async function (req, res) {
   res.header("access-control-allow-origin", "http://localhost:8081"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Method", "*");
 
+  const oldVotesFor = await getVotes(req.body.winner, req.body.loser)
+  const oldVotesAgainst = await getVotes(req.body.loser, req.body.winner)
+  console.log({oldVotesFor,oldVotesAgainst })
+  updateVoteCount(req.body.winner, req.body.loser, oldVotesFor + 1)
 
   const results = {}
-  results[req.body.winner] = 5
-  results[req.body.loser] = 2
-   console.log("post")
-   console.log("winner: " +req.body.winner)
-   console.log("loser: " + req.body.loser)
+  results[req.body.winner] = oldVotesFor+1
+  results[req.body.loser] = oldVotesAgainst
    res.status(200).send(results).end()
 
 })
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
+# setup credentials
+```
+export AWS_ACCESS_KEY_ID=<key id>
+export AWS_SECRET_ACCESS_KEY=<secret key>
+
+```
+
 # connect to ec2 instance
 `ssh -i ./drawfee-backend.pem ec2-user@ec2-13-236-184-186.ap-southeast-2.compute.amazonaws.com`
+
+
